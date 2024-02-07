12587073234dc4fae98cb5514cb085c06694a3aa
Romeootje <romeo.timmerman@gmail.com>
2024-02-07 15:46:35 +0100

    Add project files.

---
+++ F1MVC/F1Lib/Models/Circuit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Lib.Models
+{
+    public class Circuit
+    {
+        public int ID { get; set; }
+
+        [StringLength(50, ErrorMessage = "Maximumlengte van {0} is {1} tekens")]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; } = string.Empty;
+
+        public double? Latitude { get; set; }
+
+        [DataType(DataType.Url)]
+        [StringLength(250, ErrorMessage = "Maximumlengte van {0} is {1} tekens")]
+        public double? Longitude { get; set; }
+
+        public string? Wiki { get; set; } = string.Empty;
+
+        public Country? Country { get; set; }
+
+        public IEnumerable<Result> Races { get; set; } = Enumerable.Empty<Result>();
+
+    }
+}
---
+++ F1MVC/F1MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using F1Lib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace F1MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Circuit> Circuits { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Grandprix> Grandprixes { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+    }
+}
